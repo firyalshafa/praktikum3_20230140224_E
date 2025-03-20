c2208fb3d174c12fde15f87370b388460f4be3f3
firyalshafa <shafafiryal782@gmail.com>
2025-03-20 23:34:01 +0800

    Menggunakan blok using untuk memastikan koneksi database tertutup setelah digunakan

--- praktikum3/Form1.cs
+++ praktikum3/Form1.cs
@@ -122,6 +122,13 @@ namespace praktikum3
                 DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (confirm == DialogResult.Yes)
                 {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        try
+                        {
+
+                        }
+                    }
 
                 }
 
