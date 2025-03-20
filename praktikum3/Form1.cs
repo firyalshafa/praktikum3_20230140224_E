a66d23757f991590bc336af3ea08d86ffed16198
firyalshafa <shafafiryal782@gmail.com>
2025-03-20 23:18:04 +0800

    Menjalankan perintah SQL dan menyimpan jumlah baris yang terpengaruh

--- praktikum3/Form1.cs
+++ praktikum3/Form1.cs
@@ -84,6 +84,7 @@ namespace praktikum3
                         cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
                         cmd.Parameters.AddWithValue("@Telepon", txtTelepon.Text.Trim());
                         cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text.Trim());
+                        int rowsAffected = cmd.ExecuteNonQuery();
                     }
 
                 }
