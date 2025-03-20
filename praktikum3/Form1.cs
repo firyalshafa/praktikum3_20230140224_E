46ea832584ab8502f26e5f93f1d32d8db090c88f
firyalshafa <shafafiryal782@gmail.com>
2025-03-21 00:06:39 +0800

    Mengisi txtTelepon dengan nilai dari kolom keempat di DataGridView, memastikan null safety

--- praktikum3/Form1.cs
+++ praktikum3/Form1.cs
@@ -183,9 +183,11 @@ namespace praktikum3
             txtNim.Text = row.Cells[0].Value.ToString();
             txtNama.Text = row.Cells[1].Value?.ToString();
             txtEmail.Text = row.Cells[2].Value?.ToString();
-        }
+            txtTelepon.Text = row.Cells[3].Value?.ToString();
 
         }
+
+    }
 
     }
 }
