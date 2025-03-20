5cd80b2dccbf065113e4d71b8c22a9d7873fea9b
firyalshafa <shafafiryal782@gmail.com>
2025-03-20 23:16:11 +0800

    Menambahkan parameter @Email ke SqlCommand untuk menyimpan input Email

--- praktikum3/Form1.cs
+++ praktikum3/Form1.cs
@@ -81,6 +81,7 @@ namespace praktikum3
                     {
                         cmd.Parameters.AddWithValue("@Nim", txtNim.Text.Trim());
                         cmd.Parameters.AddWithValue("@Nama", txtNama.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
                     }
 
                 }
