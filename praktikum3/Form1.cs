837fa9712fddc4b69ee6ba7f5b3dd7bd78db9d19
firyalshafa <shafafiryal782@gmail.com>
2025-03-21 00:17:11 +0800

    Membuat objek SqlCommand untuk mengeksekusi query

--- praktikum3/Form1.cs
+++ praktikum3/Form1.cs
@@ -123,6 +123,7 @@ namespace praktikum3
                     }
                     conn.Open();
                     string query = "UPDATE Mahasiswa SET Nama = @Nama, Email = @Email, Telepon = @Telepon, Alamat = @Alamat WHERE NIM = @NIM";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
                 }
             }
 
